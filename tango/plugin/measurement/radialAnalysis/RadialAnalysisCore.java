6f565384c0a799be8f5437bf027a1dbe009bc203
Thomas Boudier <thomas.boudier@upmc.fr>
2015-10-15 13:00:20 +0800

    style

--- tango/plugin/measurement/radialAnalysis/RadialAnalysisCore.java
+++ tango/plugin/measurement/radialAnalysis/RadialAnalysisCore.java
@@ -34,50 +34,55 @@ import tango.dataStructure.SegmentedCellImages;
  *
  * @author Jean Ollion
  */
-
 public class RadialAnalysisCore {
+
     ImageFloat distanceMap;
     ImageInt mask;
     float[] sortedDistances;
     boolean verbose;
     int nbCPUs;
-    
+
     public RadialAnalysisCore(ImageHandler nucleusIntensity, ImageInt mask, ImageInt segmentedStructure, boolean inside, int nbCPUs, boolean verbose) {
-        this.verbose=verbose;
-        this.nbCPUs=nbCPUs;
-        this.mask=new BlankMask(mask);
+        this.verbose = verbose;
+        this.nbCPUs = nbCPUs;
+        this.mask = new BlankMask(mask);
         initDistanceMap(segmentedStructure, inside);
         initIndicies(null, mask, segmentedStructure, inside);
     }
-    
+
     public RadialAnalysisCore(InputCellImages inputImages, SegmentedCellImages images, int structureIdx, int nbCPUs, boolean verbose) {
-        this.verbose=verbose;
-        this.nbCPUs=nbCPUs;
-        this.mask=new BlankMask(images.getImage(0));
+        this.verbose = verbose;
+        this.nbCPUs = nbCPUs;
+        this.mask = new BlankMask(images.getImage(0));
         this.distanceMap = images.getDistanceMap(structureIdx, nbCPUs);
-        if (verbose) distanceMap.showDuplicate("distance map");
-        initIndicies(null, images.getImage(0), images.getImage(structureIdx), structureIdx==0);
+        if (verbose) {
+            distanceMap.showDuplicate("distance map");
+        }
+        initIndicies(null, images.getImage(0), images.getImage(structureIdx), structureIdx == 0);
     }
-    
-    
+
     protected void initDistanceMap(ImageInt segmentedStructure, boolean inside) {
-        distanceMap=EDT.run(segmentedStructure, 0f, (float)mask.getScaleXY(), (float)mask.getScaleZ(), !inside, nbCPUs);
-        if (verbose) distanceMap.showDuplicate("radial analysis:distance map");
-        
+        distanceMap = EDT.run(segmentedStructure, 0f, (float) mask.getScaleXY(), (float) mask.getScaleZ(), !inside, nbCPUs);
+        if (verbose) {
+            distanceMap.showDuplicate("radial analysis:distance map");
+        }
+
     }
-    
+
     protected int getVolume(ImageInt maskImage, ImageInt segmentedStructure, boolean inside) {
-        int count=0;
+        int count = 0;
         if (inside) {
-            for (int z = 0; z<segmentedStructure.sizeZ; z++) {
-                for (int xy=0; xy<segmentedStructure.sizeXY; xy++) {
-                    if (segmentedStructure.getPixelInt(xy, z) !=0) count++;
+            for (int z = 0; z < segmentedStructure.sizeZ; z++) {
+                for (int xy = 0; xy < segmentedStructure.sizeXY; xy++) {
+                    if (segmentedStructure.getPixelInt(xy, z) != 0) {
+                        count++;
+                    }
                 }
             }
         } else { //compute nucleusMask - segmentedStructure
-            for (int z = 0; z<segmentedStructure.sizeZ; z++) {
-                for (int xy=0; xy<segmentedStructure.sizeXY; xy++) {
-                    if (maskImage.getPixelInt(xy, z)!=0 && segmentedStructure.getPixelInt(xy, z)==0) {
+            for (int z = 0; z < segmentedStructure.sizeZ; z++) {
+                for (int xy = 0; xy < segmentedStructure.sizeXY; xy++) {
+                    if (maskImage.getPixelInt(xy, z) != 0 && segmentedStructure.getPixelInt(xy, z) == 0) {
                         count++;
                     }
                 }
@@ -85,27 +90,25 @@ public class RadialAnalysisCore {
         }
         return count;
     }
-    
 
-    
     protected void initIndicies(ImageHandler nucelucIntensity, ImageInt maskImage, ImageInt segmentedStructure, boolean inside) {
         int volume = getVolume(maskImage, segmentedStructure, inside);
-        this.sortedDistances=new float[volume];
-        int count=0;
+        this.sortedDistances = new float[volume];
+        int count = 0;
         if (inside) {
-            for (int z = 0; z<distanceMap.sizeZ; z++) {
-                for (int xy=0; xy<distanceMap.sizeXY; xy++) {
-                    if (segmentedStructure.getPixelInt(xy, z) !=0) {
-                        sortedDistances[count]=distanceMap.getPixel(xy, z);
+            for (int z = 0; z < distanceMap.sizeZ; z++) {
+                for (int xy = 0; xy < distanceMap.sizeXY; xy++) {
+                    if (segmentedStructure.getPixelInt(xy, z) != 0) {
+                        sortedDistances[count] = distanceMap.getPixel(xy, z);
                         count++;
                     }
                 }
             }
         } else {
-            for (int z = 0; z<distanceMap.sizeZ; z++) {
-                for (int xy=0; xy<distanceMap.sizeXY; xy++) {
-                    if (maskImage.getPixelInt(xy, z)!=0 && segmentedStructure.getPixelInt(xy, z)==0) {
-                        sortedDistances[count]=distanceMap.getPixel(xy, z);
+            for (int z = 0; z < distanceMap.sizeZ; z++) {
+                for (int xy = 0; xy < distanceMap.sizeXY; xy++) {
+                    if (maskImage.getPixelInt(xy, z) != 0 && segmentedStructure.getPixelInt(xy, z) == 0) {
+                        sortedDistances[count] = distanceMap.getPixel(xy, z);
                         count++;
                     }
                 }
@@ -113,65 +116,78 @@ public class RadialAnalysisCore {
         }
         Arrays.sort(sortedDistances);
     }
-    
 
-    
-    protected double getShell(float  distance) {
-        if (distance==0 || sortedDistances.length==0) return 0;
+    protected double getShell(float distance) {
+        if (distance == 0 || sortedDistances.length == 0) {
+            return 0;
+        }
         int idx = Arrays.binarySearch(sortedDistances, distance);
-        if (idx<0) return ((double)(-idx+1)/(double)sortedDistances.length);
+        if (idx < 0) {
+            return ((double) (-idx + 1) / (double) sortedDistances.length);
+        }
         // case no structure --> constant image
-        if(sortedDistances[0]==sortedDistances[sortedDistances.length-1]) return -1;
-        else {
+        if (sortedDistances[0] == sortedDistances[sortedDistances.length - 1]) {
+            return Double.NaN;
+        } else {
            //recherche des bornes a gauche et à droite
-           // on prend le milieu
-           int left = idx;
-           while (left>0 && sortedDistances[left]==distance) left--;
-           int right = idx;
-           while (right<(sortedDistances.length-1) && sortedDistances[right]==distance) right++;
-           return ((double)(right+left)/2d)/(double)sortedDistances.length;
+            // on prend le milieu
+            int left = idx;
+            while (left > 0 && sortedDistances[left] == distance) {
+                left--;
+            }
+            int right = idx;
+            while (right < (sortedDistances.length - 1) && sortedDistances[right] == distance) {
+                right++;
+            }
+            return ((double) (right + left) / 2d) / (double) sortedDistances.length;
         }
     }
-    
+
     public double getShell(double x, double y, double z) { // returns proportion of volume
-        float distance = distanceMap.getPixel((float)x, (float)y, (float)z, mask);
+        float distance = distanceMap.getPixel((float) x, (float) y, (float) z, mask);
         return getShell(distance);
     }
-    
+
     public double getShell(int x, int y, int z) { // returns proportion of volume
         float distance = distanceMap.getPixel(x, y, z, mask);
         return getShell(distance);
     }
-    
+
     public double getShell(Point3D p) {
         float distance = distanceMap.getPixelInterpolated(p);
         return getShell(distance);
     }
-    
+
     public double getMinShell(Object3DVoxels o) {
         float dmin = Float.MAX_VALUE;
         for (Voxel3D v : o.getContours()) {
             float dist = distanceMap.getPixel(v.getRoundX(), v.getRoundY(), v.getRoundZ());
-            if (dist<dmin) dmin=dist;
+            if (dist < dmin) {
+                dmin = dist;
+            }
         }
         return getShell(dmin);
     }
-    
+
     public double getMaxShell(Object3DVoxels o) {
         float dmax = -Float.MAX_VALUE;
         for (Voxel3D v : o.getContours()) {
             float dist = distanceMap.getPixel(v.getRoundX(), v.getRoundY(), v.getRoundZ());
-            if (dist>dmax) dmax=dist;
+            if (dist > dmax) {
+                dmax = dist;
+            }
         }
         return getShell(dmax);
     }
-    
+
     public double getMeanShell(Object3DVoxels o) {
         double d = 0;
         for (Voxel3D v : o.getVoxels()) {
             d += distanceMap.getPixel(v.getRoundX(), v.getRoundY(), v.getRoundZ());
         }
-        if (o.getVolumePixels()>0) d/=(double)o.getVolumePixels();
-        return getShell((float)d);
+        if (o.getVolumePixels() > 0) {
+            d /= (double) o.getVolumePixels();
+        }
+        return getShell((float) d);
     }
 }
