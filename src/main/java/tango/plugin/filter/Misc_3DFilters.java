46322b3853b2a938647e5234b0343b6806599cdf
mcib3d <thomas.boudier@upmc.fr>
2016-07-27 14:43:32 +0800

    Implementing PureDenoise Need to use new jar for Droplet3D bandPass3D

--- src/main/java/tango/plugin/filter/Misc_3DFilters.java
+++ src/main/java/tango/plugin/filter/Misc_3DFilters.java
@@ -1,5 +1,7 @@
 package tango.plugin.filter;
 
+import denoise.Denoising;
+import denoise.Operations;
 import ebmoll.lipid_droplet_counter.filters.Bandpass3D;
 import ij.IJ;
 import ij.ImagePlus;
@@ -9,31 +11,35 @@ import ij.process.ByteProcessor;
 import ij.process.ImageConverter;
 import ij.process.ImageProcessor;
 import ij.process.ShortProcessor;
-import java.util.HashMap;
+import imageware.Builder;
+import imageware.ImageWare;
 import mcib3d.image3d.ImageHandler;
 import tango.dataStructure.InputImages;
 import tango.parameter.*;
 
+import java.util.HashMap;
+
+import static tango.spatialStatistics.SDIEvaluator.filter1d.Filter1d.filters;
+
 /**
- *
- **
+ * *
  * /**
  * Copyright (C) 2008- 2012 Thomas Boudier and others
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * This file is part of mcib3d
- *
+ * <p>
  * mcib3d is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 3 of the License, or (at your option) any later
  * version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  *
@@ -45,6 +51,10 @@ public class Misc_3DFilters implements PreFilter {
     static final int LOG = 1;
     static final int DENOISE = 2;
     static final int BANDPASS = 3;
+    static String gauss3DHelp = "<ul><li><strong>Gaussian 3D</strong> taken from ImageJ Process/Filters.</li></ul>";
+    static String log3DHelp = "<ul><li><strong>LoG</strong>, laplacian of Gaussian, <br>taken from BIG http://bigwww.epfl.ch/sage/soft/LoG3D/ <br>. <br>When using this plugin, please cite : <br><br>D. Sage, F.R. Neumann, F. Hediger, S.M. Gasser, M. Unser, \"Automatic Tracking of Individual Fluorescence Particles: Application to the Study of Chromosome Dynamics,\" IEEE Transactions on Image Processing, vol. 14, no. 9, pp. 1372-1383, September 2005.<br> </li></ul>";
+    static String denoiseHelp = "<ul><li><strong>PureDenoise</strong>, fluorescence denoising, <br>taken from BIG http://bigwww.epfl.ch/algorithms/denoise/ <br>Please make sure you have installed this plugin first</li></ul>";
+    static String BPHelp = "<ul><li><strong>BandPass</strong>, filter pixels based on object size, <br>taken from Droplet Finder http://imagejdocu.tudor.lu/doku.php?id=plugin:analysis:droplet_counter:start <br>Please make sure you have installed this plugin first</li></ul>";
     boolean debug = true;
     boolean thread = false;
     int nbCPUs = 1;
@@ -73,14 +83,10 @@ public class Misc_3DFilters implements PreFilter {
     };
     ConditionalParameter cond = new ConditionalParameter("Filter", filter_P, map);
     Parameter[] parameters = new Parameter[]{cond};
-    static String gauss3DHelp = "<ul><li><strong>Gaussian 3D</strong> taken from ImageJ Process/Filters.</li></ul>";
-    static String log3DHelp = "<ul><li><strong>LoG</strong>, laplacian of Gaussian, <br>taken from BIG http://bigwww.epfl.ch/sage/soft/LoG3D/ <br>. <br>When using this plugin, please cite : <br><br>D. Sage, F.R. Neumann, F. Hediger, S.M. Gasser, M. Unser, \"Automatic Tracking of Individual Fluorescence Particles: Application to the Study of Chromosome Dynamics,\" IEEE Transactions on Image Processing, vol. 14, no. 9, pp. 1372-1383, September 2005.<br> </li></ul>";
-    static String denoiseHelp = "<ul><li><strong>PureDenoise</strong>, fluorescence denoising, <br>taken from BIG http://bigwww.epfl.ch/algorithms/denoise/ <br>Please make sure you have installed this plugin first</li></ul>";
-    static String BPHelp = "<ul><li><strong>BandPass</strong>, filter pixels based on object size, <br>taken from Droplet Finder http://imagejdocu.tudor.lu/doku.php?id=plugin:analysis:droplet_counter:start <br>Please make sure you have installed this plugin first</li></ul>";
     // contructor for Tango
 
     public Misc_3DFilters() {
-        filter_P.setHelp("Availabe filters are : " + gauss3DHelp + log3DHelp + /*denoiseHelp +*/ BPHelp, true);
+        filter_P.setHelp("Available filters are : " + gauss3DHelp + log3DHelp + denoiseHelp + BPHelp, true);
         voisXY_P.setHelp("The radius in <em>X</em> and <em>Y</em> direction", true);
         voisZ_P.setHelp("The radius in <em>Z</em> direction", true);
         iteration_P.setHelp("Number of iterations for PureDenoise", true);
@@ -92,36 +98,16 @@ public class Misc_3DFilters implements PreFilter {
     private ImageHandler process(ImageHandler ih) {
         if (filter == GAUSSIAN) {
             ImageHandler img2 = ih.duplicate();
-            //img2.show();
             ij.plugin.GaussianBlur3D.blur(img2.getImagePlus(), voisx, voisx, voisz);
-            //IJ.run("Gaussian Blur 3D...", "x=" + voisx + " y=" + voisx + " z=" + voisz);  IJ.log("finished");
-            
+
             img2.setTitle(ih.getTitle() + "::Gauss3D");
             return img2;
         } else if (filter == LOG) {
             return LaplacianOfGaussian3D.LOG(ih, voisx, voisz);
 
         } else if (filter == DENOISE) {
-            // TODO ne pas utiliser le window manager car en general process en batch
-            // OK, mais alors le supprimer de la liste, 
-            // et reflechir a comment l´utiliser car tres performant
-            /*
-            if(!imp.isVisible())imp.show();
-            int nb = WindowManager.getImageCount();
-            IJ.run("PureDenoise ...", "parameters='1 " + cs + "' estimation='Auto Global'");            
-            while (WindowManager.getWindowCount() == nb) {
-                IJ.wait(100);
-            }
-            if (this.debug) {
-                IJ.log("finished");
-            }
-            ImagePlus res = IJ.getImage();
-            //res.hide();
-            */
-            return ih;
+            return pureDenoise(ih);
         } else if (filter == BANDPASS) {
-            //ImageFloat iflo = new ImageFloat(ImageHandler.wrap(imp));
-            //iflo.showDuplicate("converted float image");
             ImagePlus imp = ih.getImagePlus();
             new ImageConverter(imp).convertToGray32();
             imp.updateAndRepaintWindow();
@@ -144,10 +130,7 @@ public class Misc_3DFilters implements PreFilter {
             imp.setStack(convert);
             Calibration cal = imp.getCalibration();
             double ratio = cal.pixelDepth / cal.pixelWidth;
-            //IJ.log("depth=" + imp.getBitDepth() + " " + (imp.getStack().getProcessor(1) instanceof FloatProcessor));
-            //IJ.run(imp, "DF_Bandpass", "maximal_feature_size=" + maxs + " minimal_feature_size=" + mins + " z/x_aspect_ratio=" + ratio);
-            
-            
+
             Bandpass3D bp3d = new Bandpass3D();
             bp3d.in_hprad = maxs;
             bp3d.in_lprad = mins;
@@ -166,13 +149,67 @@ public class Misc_3DFilters implements PreFilter {
     public ImageHandler runPreFilter(int currentStructureIdx, ImageHandler input, InputImages images) {
         filter = filter_P.getSelectedIndex();
         voisx = voisXY_P.getDoubleValue(voisx);
-        if (useScale.isSelected()) voisz = voisx * input.getScaleXY()/input.getScaleZ();
+        if (useScale.isSelected()) voisz = voisx * input.getScaleXY() / input.getScaleZ();
         else voisz = voisZ_P.getDoubleValue(voisz);
         cs = iteration_P.getValue();
         mins = mins_P.getIntValue(mins);
         maxs = maxs_P.getIntValue(maxs);
 
         return process(input);
+    }
+
+    // From PureDenoise_
+    // Florian Luisier
+    // Biomedical Imaging Group (BIG)
+    // Ecole Polytechnique Federale de Lausanne (EPFL)
+    // Lausanne, Switzerland
+    //
+    // Information:
+    // http://bigwww.epfl.ch/algorithms/denoise/
+
+    private ImageHandler pureDenoise(ImageHandler input) {
+        ImagePlus impSource = input.getImagePlus();
+
+        int nx = impSource.getWidth();
+        int ny = impSource.getHeight();
+
+        ImageWare original = Builder.create(impSource);
+        if (nx < 16 || ny < 16) {
+            IJ.log("The size of your data is inappropriate.");
+        }
+
+        IJ.log("Denoising in progress...");
+        original = original.convert(ImageWare.DOUBLE);
+        int nz = original.getSizeZ();
+        int[] Ext = new int[2];
+        int nxe = (int) (Math.ceil((double) nx / 16) * 16);
+        int nye = (int) (Math.ceil((double) ny / 16) * 16);
+        if (nxe != nx || nye != ny) {
+            original = Operations.symextend2D(original, nxe, nye, Ext);
+        } else {
+            Ext[0] = 0;
+            Ext[1] = 0;
+        }
+        double[] AlphaHat = new double[nz];
+        double[] DeltaHat = new double[nz];
+        double[] SigmaHat = new double[nz];
+
+        // parameters
+        boolean FRAMEWISE = false;
+        int CS = iteration_P.getValue();
+        int NBFRAME = 1;
+
+        Denoising denoising = new Denoising(original, AlphaHat, DeltaHat, SigmaHat, FRAMEWISE, CS, NBFRAME);
+        denoising.setLog(false);
+        denoising.setFramewise(FRAMEWISE);
+        denoising.estimateNoiseParameters();
+        denoising.setCycleSpins(CS);
+        denoising.setMultiFrame(NBFRAME);
+        denoising.perform();
+        ImageWare output = denoising.getOutput();
+        ImageStack stack = output.buildImageStack();
+
+        return ImageHandler.wrap(stack);
     }
 
     public void setVerbose(boolean debug) {
